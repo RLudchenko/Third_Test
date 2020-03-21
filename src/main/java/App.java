e10e7ddbf402c47efc305bd3914eb2008d22a823
Ludchenko <skillet367@icloud.com>
2020-03-21 18:35:48 +0200

    added methods sum() and mul()

---
+++ src/main/java/App.java
@@ -0,0 +1,16 @@
+public class App {
+    public static void main(String[] args) {
+        int a = 100;
+        int b = 20;
+        System.out.println(sum(a, b));
+        System.out.println(mul(a, b));
+    }
+
+    public static int sum(int a, int b) {
+        return a * b;
+    }
+
+    public static int mul(int a, int b) {
+        return a * b;
+    }
+}
